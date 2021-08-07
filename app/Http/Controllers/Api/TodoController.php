f034395a1f17475df02e4d1f7cf5bd81782e3415
daking24 <daking3968@outlook.com>
2021-08-07 18:01:47 +0100

    Messages

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -19,25 +19,61 @@ class TodoController extends Controller
                 'description' => $request['description'],
                 'user_id' => $request['user_id'],
             ]);
+
+            if ($todo){
+                return response()->json(['message' => 'Added succesfully'], 201);
+            }
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()]);
+            return response()->json(['error' => $e->getMessage()], 442);
         }
     }
 
     public function update(Request $request, Todo $todo)
     {
-        $todo->update($request->all());
+        try {
+            $todo->update($request->all());
+
+            if ($todo){
+                return response()->json(['message' => 'Updated succesfully'], 200);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 
     public function completed(Request $request, Todo $todo) {
-        $todo->update([
-            'is_completed' => true,
-            'completed_at' => Carbon::now()
-        ]);
+        try {
+            $todo->update([
+                'is_completed' => true,
+                'completed_at' => Carbon::now()
+            ]);
+            if ($todo){
+                return response()->json(['message' => 'Updated succesfully'], 200);
+            }
+        } catch(\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 
     public function destroy(Todo $todo)
     {
-        $todo->delete();
+        try {
+            $todo->delete();
+
+            if ($todo) {
+                return response()->json(['message' => 'Deleted succesfully'], 200);
+            }
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ]);
+        }
+
+
     }
 }
